15ce05ff90bdf65840535e3bb0acbe5a4cdff31e
huojie <stophemo@outlook.com>
2024-01-25 16:48:29 +0800

    Initial commit

---
+++ server-c/src/main/java/com/example/serverc/ServerCApplication.java
@@ -0,0 +1,13 @@
+package com.example.serverc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerCApplication.class, args);
+    }
+
+}
